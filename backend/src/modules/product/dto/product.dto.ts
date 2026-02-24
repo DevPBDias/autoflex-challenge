e930bbcfb1c9bf2babf7b35c99a82f33c270959f
devpbdias <devpbdias@gmail.com>
2026-02-24 18:39:26 -0300

    feat/refactor: add new packages zod for validation, jest for tests and cors for integration with frontend. Refactor product dto with zod

--- backend/src/modules/product/dto/product.dto.ts
+++ backend/src/modules/product/dto/product.dto.ts
@@ -1,11 +1,39 @@
-export interface CreateProductDTO {
-  code: string;
-  name: string;
-  value: number;
-}
+import { z } from "zod";
 
-export interface UpdateProductDTO {
-  code?: string;
-  name?: string;
-  value?: number;
-}
+export const CreateProductSchema = z.object({
+  code: z
+    .string({ message: "Field 'code' must be a non-empty string" })
+    .trim()
+    .min(1, "Field 'code' must be a non-empty string"),
+  name: z
+    .string({ message: "Field 'name' must be a non-empty string" })
+    .trim()
+    .min(1, "Field 'name' must be a non-empty string"),
+  value: z
+    .number({
+      message: "Field 'value' must be a positive number",
+    })
+    .min(0, "Field 'value' must be a positive number"),
+});
+
+export const UpdateProductSchema = z.object({
+  code: z
+    .string({ message: "Field 'code' must be a non-empty string" })
+    .trim()
+    .min(1, "Field 'code' must be a non-empty string")
+    .optional(),
+  name: z
+    .string({ message: "Field 'name' must be a non-empty string" })
+    .trim()
+    .min(1, "Field 'name' must be a non-empty string")
+    .optional(),
+  value: z
+    .number({
+      message: "Field 'value' must be a positive number",
+    })
+    .min(0, "Field 'value' must be a positive number")
+    .optional(),
+});
+
+export type CreateProductDTO = z.infer<typeof CreateProductSchema>;
+export type UpdateProductDTO = z.infer<typeof UpdateProductSchema>;
